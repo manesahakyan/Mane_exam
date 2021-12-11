7e2d6287b9d721bad0c9d28dc0f25a7eaf20df78
manesahakyan <89680204+manesahakyan@users.noreply.github.com>
2021-12-11 15:50:56 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let student =[
+let student = [
     { "first_name": "Armen", "last_name": "Amiryan", "age": "33" },
     { "first_name": "Karine", "last_name": "Karapetyan", "age": "23" },
     { "first_name": "Manushak", "last_name": "Voskanyan", "age": "20" },
@@ -7,16 +7,8 @@ let student =[
     { "first_name": "Zarzand", "last_name": "Mnatsakanyan", "age": "66" },
     { "first_name": "Murad", "last_name": "Muradyan", "age": "58" },
     { "first_name": "Ashkhen", "last_name": "Khanamiryan", "age": "48" }
-]
-let newList=[]
-
-function newStudentList(student){
-      for (let i = 0; i < student.length; i++){
-          const element = student[i];
-          newList.push(element.first_name + " " + element.last_name)
-      }
-      return newList
-}
-console.log( newStudentList(student))
-
-
+  ]
+let newList = student.filter((stu) =>{
+    return stu.age > 20
+})
+console.log(newList)
