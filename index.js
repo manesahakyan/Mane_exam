225ee0d7713e3ac9c72b6a145236c97bbee0e4f5
manesahakyan <89680204+manesahakyan@users.noreply.github.com>
2021-12-11 15:38:24 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-let arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+let student =[
+    { "first_name": "Armen", "last_name": "Amiryan", "age": "33" },
+    { "first_name": "Karine", "last_name": "Karapetyan", "age": "23" },
+    { "first_name": "Manushak", "last_name": "Voskanyan", "age": "20" },
+    { "first_name": "Eghiazar", "last_name": "Avetisyan", "age": "18" },
+    { "first_name": "Hrant", "last_name": "Tokhatyan", "age": "22" },
+    { "first_name": "Zarzand", "last_name": "Mnatsakanyan", "age": "66" },
+    { "first_name": "Murad", "last_name": "Muradyan", "age": "58" },
+    { "first_name": "Ashkhen", "last_name": "Khanamiryan", "age": "48" }
+]
+let newList=[]
 
-let odds = arr.filter(n => n%2)
+function newStudentList(student){
+      for (let i = 0; i < student.length; i++){
+          const element = student[i];
+          newList.push(element.first_name + " " + element.last_name)
+      }
+      return newList
+}
+console.log( newStudentList(student))
 
-    
-    console.log(odds)
+
